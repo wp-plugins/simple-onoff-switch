97683db0953139ec26d751c3db73209c913a2f95
mattbeekler <mattbeekler@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-16 09:27:33 +0000

    Initial Commit
    git-svn-id: https://plugins.svn.wordpress.org/simple-onoff-switch/trunk@698383 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,25 @@
+=== Simple On/Off Switch ===
+Contributors: Matthew Buechler
+Donate link: http://mattbuechler.com/donate
+Tags: dashboard, admin, widgets,select, switch
+Requires at least: 3.5.1
+Tested up to: 3.5.1
+Stable tag: 3.5.1
+
+== Description ==
+
+Adds a dashboard widget with a on/off switch that returns "on" or "off" value with a shortcode. Shortcode is [simpleswitch]
+
+
+== Installation ==
+
+Upload the plugin to your blog, activate it, go to 'Dashboard'. us [simpleswitch] shortcode.
+
+= Questions? =
+
+Email matt.buechler@gmail.com
+
+== Changelog ==
+
+= 1.0 =
+First upload
---
+++ simple-on-off-switch.php
@@ -0,0 +1,106 @@
+<?php
+/*
+Plugin Name: Simple On/Off Switch
+Plugin URI: http://mattbuechler.com/portfolio/wordpress-plugins/simple-onoff-switch/
+Description: A simple plugin that returns an "On" or "Off" value via a Dashbaord Widget
+Version: 1.0
+Author: Matthew Buechler
+Author URI: http://mattbuechler.com
+License: GPL2
+*/
+
+/*  Copyright 2013  Matthew Buechler  (email : matt.buechler@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+add_option('simpleswitch', 'on');
+add_option('simpleswitch_onvalue', 'On Value');
+add_option('simpleswitch_offvalue', 'Off Value');
+
+// Create the function to output the contents of our Dashboard Widget
+
+function simpleonoffswitch_form(){ //Function for the On / Off Radio Buttons Form
+	?> 
+    <form name="sssubmit" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+    <label><input name="simpleswitch" type="radio" value="simpleswitchon" <?php if(get_option('simpleswitch') == "on") { echo 'checked'; } ?>/> On</label>
+	<label><input name="simpleswitch" type="radio" value="simpleswitchoff" <?php if(get_option('simpleswitch') == "off") { echo 'checked'; } ?>/> Off</label> &nbsp; 
+    <input name="sssubmit" type="submit" value="Save" class="button-primary" /></form>
+
+  <?php
+}
+function simpleonoffswitch_form_values() { // Function for the Values form
+		$ss_onvalueset = get_option('simpleswitch_onvalue');
+		$ss_offvalueset = get_option('simpleswitch_offvalue');
+	?>
+	   <hr />
+    <form name="ssvalues" action="<?php echo $_SERVER['PHP_SELF'] ?>?ssvalues_submit" method="post">
+    <label>On Value <input name="simpleswitch_onvalue" type="input" value="<?php echo $ss_onvalueset; ?>" size="60" /></label><br />
+    <label>Off Value <input name="simpleswitch_offvalue" type="input" value="<?php echo $ss_offvalueset; ?>" size="60" /></label><br />
+    <input type="hidden" name="values_submit" value="true" /> 
+    <input name="submit" type="submit" value="Save" class="button-primary" />
+    </form> 
+	<?php
+}
+
+function simpleonoffswitch_dashboard_widget_function() { //show the dashboard widget and include the functions
+	// Display whatever it is you want to show
+	simpleswitch_set();
+	$values_submit = $_POST['values_submit'];
+	if(isset($_POST['ssvalues_submit'])); {
+	simpleswitch_values_set();
+	}
+	echo simpleonoffswitch_form();
+	echo simpleonoffswitch_form_values();
+} 
+
+function simpleswitch_set() { //set the status of the radio buttons on or off
+	$switchstatus = $_POST['simpleswitch'];
+	if(isset($_POST['sssubmit'])) {
+	if($switchstatus == "simpleswitchon") {
+		update_option('simpleswitch', 'on'); }
+	if($switchstatus == "simpleswitchoff") {
+		update_option('simpleswitch', 'off');
+		} }
+function simpleswitch_values_set() { // set the values
+	if(isset($_POST['values_submit'])) {
+	$on_value = $_POST['simpleswitch_onvalue'];
+	$off_value = $_POST['simpleswitch_offvalue'];
+	
+	update_option('simpleswitch_onvalue', $on_value);
+	update_option('simpleswitch_offvalue', $off_value);
+		
+}	}
+}
+function simpleswitch_shortcode() { // display value dependant on status of the switch shorcode
+	if(get_option('simpleswitch') == "on") {
+		echo get_option('simpleswitch_onvalue');
+	} 
+	if(get_option('simpleswitch') == "off") {
+		echo get_option('simpleswitch_offvalue');
+	}	
+}
+add_shortcode('simpleswitch', 'simpleswitch_shortcode'); //set the shortcode
+
+// Create the function use in the action hook
+
+function example_add_dashboard_widgets() {
+	wp_add_dashboard_widget('simpleonoffswitch_dashboard_widget', 'Simple On/Off Switch', 'simpleonoffswitch_dashboard_widget_function');	
+}
+// Hook into the 'wp_dashboard_setup' action to register our other functions
+add_action('wp_dashboard_setup', 'example_add_dashboard_widgets' ); // Hint: For Multisite Network Admin Dashboard use wp_network_dashboard_setup instead of wp_dashboard_setup.
+
+
+?>
